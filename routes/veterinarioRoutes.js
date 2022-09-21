691417ca35c16b39af87d18952266dff79f0dddd
unknown <jerez4959@gmail.com>
2022-09-21 15:21:23 -0300

    first commit

---
+++ routes/veterinarioRoutes.js
@@ -0,0 +1,34 @@
+import express from 'express';
+const router = express.Router();
+import {
+    registrar,
+    perfil, 
+    confirmar,
+    autenticar,
+    olvidePassword,
+    comprobarToken,
+    nuevoPassword,
+    actualizarPerfil,
+    actualizarPassword 
+}from '../controllers/veterinarioControllers.js'
+import checAuth from '../middleware/authMiddleware.js'
+
+
+// plubico
+router.post('/', registrar);
+router.get('/confirmar/:token', confirmar);
+router.post('/login', autenticar);
+router.post('/olvide-password', olvidePassword);
+router.route('/olvide-password/:token').get(comprobarToken).post(nuevoPassword);
+
+
+
+//privado
+router.get('/perfil',checAuth, perfil);
+router.put('/perfil/:id',checAuth,actualizarPerfil)
+router.put('/actualizar-Password',checAuth,actualizarPassword )
+
+
+
+
+export default router;
